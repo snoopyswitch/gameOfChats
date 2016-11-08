9d4176fb9d28af56c839d109889916aa42dab1ad
Mladen Vidovic <snoopyswitch@gmail.com>
2016-11-08 12:50:23 +0100

    cache images

---
+++ GameOfChats/Extensions.swift
@@ -0,0 +1,54 @@
+//
+//  Extensions.swift
+//  GameOfChats
+//
+//  Created by Mladen Vidović on 08/11/2016.
+//  Copyright © 2016 Mladen Vidović. All rights reserved.
+//
+
+import UIKit
+
+
+let imageCache = NSCache<AnyObject, AnyObject>()
+
+
+extension UIImageView {
+    
+    func loadImageUsingCacheWithUrlString (urlString: String) {
+        
+        self.image = nil
+        
+        //check cache for image first
+        if let cachedImage = imageCache.object(forKey: urlString as AnyObject) as? UIImage {
+            self.image = cachedImage
+            return
+        }
+        
+        
+        
+        let url = URL(string: urlString)
+        
+        URLSession.shared.dataTask(with: url!, completionHandler: { (data, response, error) in
+            
+            if error != nil {
+                print(error!)
+                return
+            }
+            
+            
+            DispatchQueue.main.async {
+                
+                if let downloadedImage = UIImage(data: data!) {
+                    imageCache.setObject(downloadedImage, forKey: urlString as AnyObject)
+                    
+                    self.image = downloadedImage
+                }
+                
+            }
+            
+            
+        }).resume()
+        
+    }
+    
+}
